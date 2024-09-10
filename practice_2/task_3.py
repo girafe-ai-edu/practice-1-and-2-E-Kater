885ef725d219a84fff6b691777486a2427227860
ekaterina <es.ekaterina@gmail.com>
2024-09-10 22:16:03 +0300

    Homework1 Ekaterina

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Write a program to calculate the body mass index (BMI) of a person. The user must enter their height and weight, after which you use one of the formulas below to determine the index.
+Write a program to calculate the body mass index (BMI) of a person. 
+The user must enter their height and weight, after which you use one of the formulas below to determine the index.
 BMI = weight/height**2 
+Assumption: weight and height should be in kg and sm => we can read them as integer
 """
-weight = input()
-height = input()
+weight = int(input())
+height = int(input())
 
+print((weight  / height**2))
 
 #code here
 
--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Develop a program that asks the user for an integer 4-digit number and calculates the sum of its constituent digits. For example, if the user enters the number 3141, the program should output the following result:
+Develop a program that asks the user for an integer 4-digit number and calculates the sum of its constituent digits. 
+For example, if the user enters the number 3141, the program should output the following result:
 3 + 1 + 4 + 1 = 9
 
 """
+number_ = input()
+sum = 0
+for n in number_:
+    sum = sum + int(n)
+print(sum)
